3e0f633cd318ea4c6effada21ee88dcc6dfcabb2
Yasindu Nethmina <yasindunethmina1938096@gmail.com>
2023-01-07 12:22:10 +0530

    vercel deployment code!

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
+require("dotenv").config();
 require('@nomiclabs/hardhat-waffle');
-
-const privateKey = fs.readFileSync('.secret').toString().trim();
-
 module.exports = {
+  solidity: '0.8.4',
   networks: {
+    goerli: {
+      url: process.env.ENDPOINT_URL,
+      accounts: [process.env.DEPLOYER_KEY],
+
+    },
     hardhat: {
       chainId: 1337,
     },
   },
-  solidity: '0.8.4',
 };
