296a3eff0131ac8f0b12f027e935751c5e529d06
Yasindu Nethmina <yasindunethmina1938096@gmail.com>
2023-01-07 23:12:25 +0530

    update some information on the smart contract backend!

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,4 @@
 import market from './NFTMarketplace.json';
 
-export const MarketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const MarketAddress = '0xf58674c2533045a6E2FECf1c68F5AF3d783CDE7E';
 export const MarketAddressABI = market.abi;
