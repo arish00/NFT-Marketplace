2fa2d28720fe8e1b37073e582eb295fea8820bcd
Yasindu Nethmina <yasindunethmina1938096@gmail.com>
2023-01-07 22:57:37 +0530

    cors error fix! goerli network setup!

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,31 +1,15 @@
 const fs = require('fs');
 require('@nomiclabs/hardhat-waffle');
-require("dotenv").config();
-
-const privateKey = fs.readFileSync('.secret').toString().trim();
 
 module.exports = {
-  defaultNetwork: "localhost",
   networks: {
     hardhat: {
-      // chainId: 1337,
-    },
-    localhost: {
-    url:"http://127.0.0.1:8545"
+      chainId: 1337,
     },
     goerli: {
-      url: process.env.ENDPOINT_URL,
-      accounts: [process.env.DEPLOYER_KEY]
-    }
+      url: 'https://eth-goerli.g.alchemy.com/v2/nrwjWYK8BGomfvFAZ5DO_mTwwXQY8bQ_',
+      accounts: ['42897f38222710b58d7f94aa21f836079e4cdec99aed0fc827df5211eda689a3'],
+    },
   },
   solidity: '0.8.4',
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
-  },
-  paths: {
-    sources: "./contracts/"
-  }
 };
